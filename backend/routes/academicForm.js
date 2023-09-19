0f2e76258ea2122b0941eec6fe2f0d375da57986
Dev Jobalia <webdev.jobalia@gmail.com>
2023-09-19 17:15:48 +0530

    successfull upload and download

--- backend/routes/academicForm.js
+++ backend/routes/academicForm.js
@@ -7,10 +7,31 @@ const FormDataModel = require("../models/model2");
 
 const router = Router();
 
-// Configure Multer for file uploads
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
-
+const DIR = "./public/";
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    const fileName = file.originalname.toLowerCase().split(" ").join("-");
+    cb(null, "-" + fileName);
+  },
+});
+var upload = multer({
+  storage: storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype == "image/png" ||
+      file.mimetype == "image/jpg" ||
+      file.mimetype == "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only .png, .jpg and .jpeg format allowed!"));
+    }
+  },
+});
 // Handle file uploads
 router.post(
   "/",
