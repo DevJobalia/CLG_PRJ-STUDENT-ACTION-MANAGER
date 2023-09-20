20d38bb8b179ebebf0fc056b1d427c73ad75e12f
Dev Jobalia <webdev.jobalia@gmail.com>
2023-09-20 19:42:19 +0530

    upload files locally just save path on mongodb

--- backend/routes/academicForm.js
+++ backend/routes/academicForm.js
@@ -1,20 +1,35 @@
 const express = require("express");
 const multer = require("multer");
-const cloudinary = require("cloudinary").v2;
-const mongoose = require("mongoose");
 const { Router } = express;
 const FormDataModel = require("../models/model2");
+// get path
+const { dirname, join } = require("path");
+const fs = require("fs");
 
 const router = Router();
 
-const DIR = "./public/";
+// const __filename = fileURLToPath(import.meta.url);
+const appDir = dirname(require.main.filename);
+
+const saveDir = join(appDir, "public/");
+
+// Create the destination directory if it doesn't exist
+if (!fs.existsSync(saveDir)) {
+  fs.mkdirSync(saveDir, { recursive: true });
+}
+console.log(saveDir);
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, DIR);
+    cb(null, saveDir);
   },
   filename: (req, file, cb) => {
-    const fileName = file.originalname.toLowerCase().split(" ").join("-");
-    cb(null, "-" + fileName);
+    const uniqueFileName =
+      Date.now() + "-" + file.originalname.toLowerCase().split(" ").join("-");
+    // const fileName =
+    //   uniqueFileName + ;
+
+    cb(null, uniqueFileName);
   },
 });
 var upload = multer({
@@ -44,17 +59,29 @@ router.post(
     { name: "internship" },
     { name: "certification" },
   ]),
-  (req, res) => {
-    // Handle file uploads and store the files
-    // You can use req.files to access the uploaded files
-
-    // Example: Save the files to a folder (adjust the path as needed)
+  async (req, res) => {
     const competitionFile = req.files["competition"][0];
-    console.log(competitionFile);
-    // const prjPresentFile = req.files['prjPresent'][0];
-    // ...
+    const prjPresentFile = req.files["prjPresent"][0];
+    const paperPresentFile = req.files["paperPresent"][0];
+    const courseFile = req.files["course"][0];
+    const copyrightFile = req.files["copyright"][0];
+    const internshipFile = req.files["internship"][0];
+    const certificationFile = req.files["certification"][0];
 
-    // Return a response indicating success or failure
+    console.log(saveDir + "" + competitionFile.filename);
+
+    const newData = FormDataModel({
+      competition: saveDir + competitionFile.filename,
+      prjPresent: saveDir + prjPresentFile.filename,
+      paperPresent: saveDir + paperPresentFile.filename,
+      course: saveDir + courseFile.filename,
+      copyright: saveDir + copyrightFile.filename,
+      internship: saveDir + internshipFile.filename,
+      certification: saveDir + certificationFile.filename,
+    });
+
+    await newData.save();
+
     res.status(200).json({ message: "Files uploaded successfully" });
   }
 );
